adc41df21aec8be658e6aa7d05cd5f4425c1fe95
Patricio Tomas Garcia <47353690+tomas28@users.noreply.github.com>
2019-03-20 12:12:30 -0300

    do while while for c

---
+++ clase2/main.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+//switch
+switch(valor)
+{
+    case 9:
+    case 'nueve':
+
+
+}
+
+/*
+//for
+int i;
+   for(i=0;i<10;i++)
+   {
+       printf("%d\n",(i+1));
+   }
+
+
+ /*
+ //do while
+    int i;
+    i=0;
+    do
+    {
+        i++;
+        printf("%d\n",i);
+    }
+    while(i<10);
+
+
+
+ //while
+ /*  int i;
+   i=0;
+   while(i<10)
+   {
+       printf("%d\n",(i+1);
+       i=i+1;
+   }
+
+   /*float result;
+   int n1,n2;
+   n1 = 6;
+   n2 = 8;
+   result=(float)n1/n2;
+   */
+   return 0;
+}
